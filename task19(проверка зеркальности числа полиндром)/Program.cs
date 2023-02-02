366ae6be81b32b6d5fee6898affacff08de5183e
Antonik Nikita <n_antonik@mail.ru>
2023-02-02 22:34:59 +0300

    HomeWork1

--- task19(проверка зеркальности числа полиндром)/Program.cs
+++ task19(проверка зеркальности числа полиндром)/Program.cs
@@ -5,4 +5,21 @@
 //23432 -> да
 
 Console.WriteLine("Введите пятизначное число: ");
-int num = Convert.ToInt32(Console.ReadLine());
+string num = Console.ReadLine();
+int len = num.Length;
+
+if (len == 5)
+{
+    if (num[0] == num[4] && num[1] == num[3])
+    {
+        Console.WriteLine($"{num} - да");
+    }
+    if (num[0] != num[4])
+    {
+        Console.WriteLine($"{num} - нет");
+    }
+}
+else if (len != 5)
+    {
+        Console.WriteLine("Вы ввели не пятизначное число... Не надо так!");
+    }
