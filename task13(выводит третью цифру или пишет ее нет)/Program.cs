8fdec18aeb3edad2d2744c8d58d1c81891bcece4
Antonik Nikita <n_antonik@mail.ru>
2023-02-02 21:15:58 +0300

    HomeWork1

---
+++ task13(выводит третью цифру или пишет ее нет)/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int digitIndex = 2; 
+
+if (number > 100)
+{
+    int digit = number.ToString()[digitIndex] - '0';
+    Console.WriteLine("третья цифра  числа " + number + " это " + digit);
+}
+if (number < 100)
+{
+    Console.WriteLine("третьей цифры нет");
+}
---
+++ task2(выдаёт максимальное число)/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая принимает на вход три числа 
+//и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.WriteLine("Введите первое число: ");
+int numA = Convert.ToInt32(Console.ReadLine()); 
+
+Console.WriteLine("Введите второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine()); 
+
+Console.WriteLine("Введите третье число: ");
+int numC = Convert.ToInt32(Console.ReadLine()); 
+
+// Лучше делать так:
+
+int max = numA;
+
+if (numB > max) max = numB;
+if (numC > max) max = numC;
+Console.WriteLine("max = ");
+Console.WriteLine(max);
+
+//if (numA > numB && numA > numC)
+{
+    Console.WriteLine("Максимальное число: " + numA);
+}
+//if (numB > numA && numB > numC)
+{
+    Console.WriteLine("Максимальное число: " + numB);
+}
+//if (numC > numB && numC > numA)
+{
+    Console.WriteLine("Максимальное число: " + numC);
+}
