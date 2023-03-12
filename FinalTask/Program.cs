bc00a83620bc01420e3375b029c439eab6c43ad7
Antonik Nikita <n_antonik@mail.ru>
2023-03-12 23:13:38 +0300

    voidSecondArray

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -1,3 +1,20 @@
 ﻿// Написать программу, которая из имеющегося массива строк 
-// формирует массив из строк, длинна которых меньше или равна 3 символа.
+// формирует массив из строк, длинна которых меньше 
+// или равна 3 символа.
+
+string[] array1 = new string[5];
+string[] array2 = new string[array1.Length];
+
+void SecondArray(string[] array1, string[] array2) // Метод для определения длинны
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
 
